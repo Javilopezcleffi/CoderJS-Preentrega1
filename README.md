9a7a15cdbb2c290a1c9c612e96e4f5d0ad3db118
javi <javi3lopez@gmail.com>
2023-04-11 11:53:04 -0300

    calcok

--- README.md
+++
@@ -1,1 +0,0 @@
-# CoderJS-Preentrega1
--- js/main.js
+++ js/main.js
@@ -1,42 +1,74 @@
-let nombre=prompt ("Cómo es tu nombre?")
+let nombre = prompt("Cómo es tu nombre?")
 console.log(nombre)
+
+
+
 function presupuesto (largo, ancho) {
 
+  //ingresar medidas
 
-while (isNaN(largo)) {
-    largo = prompt("Por favor, ingrese el largo en centímetros:");
+  while (isNaN(largo)) {
+    largo = parseFloat (prompt("Por favor, ingrese el largo en centímetros:", "40.5"));
   }
-  
+
   while (isNaN(ancho)) {
-    ancho = prompt("Ingrese el ancho en centímetros:");
+    ancho = parseFloat (prompt("Ingrese el ancho en centímetros:", "90.3"));
   }
+
+  // variantes de molduras
+
+  const molduras = [
+    { nombre: "chica", precio: 190, codigo: "30", id: 1 },
+    { nombre: "mediana", precio: 230, codigo: "30G", id: 2 },
+    { nombre: "caja", precio: 230, codigo: "230", id: 3 },
+    { nombre: "grande", precio: 440, codigo: "270", id: 4 }
+  ];
+
+  //elegir moldura
+
+  const elegirMoldura = parseInt(prompt(`selecciona una moldura entre el 1 y el ${molduras.length}:`));
+
+  let molduraElegida;
+
+  if (elegirMoldura > 0 && elegirMoldura <= molduras.length) {
+    molduraElegida = molduras.find(m=>m.id===elegirMoldura);
+
+  } else {
+
+    return;
+  }
+
+
+  const costoVidrio = parseFloat(ancho * largo / 6.4)
+  const perimetro = parseFloat (2 * ancho + 2 * largo)
+  const costoOtros = 100
+  let costoMoldura;
+  const precioMoldura = (molduraElegida.precio);
+
   
-const costoVidrio = ancho * largo / 6.4
-const perimetro = 2 * ancho + 2 * largo 
-let costoMoldura
-const precioMoldura = 200 
-const costoOtros = 100
 
-if (perimetro<244) {
-costoMoldura = precioMoldura
-} else if (perimetro >= 244 && perimetro <= 488) {
-costoMoldura = (2 * precioMoldura);
-  } else 
-  costoMoldura = 3 * precioMoldura;
+  if (perimetro < 244) {
+    costoMoldura = precioMoldura
+  } else if (perimetro >= 244 && perimetro <= 488) {
+    costoMoldura = (2 * precioMoldura);
+  } else
+    costoMoldura = 3 * precioMoldura;
 
-let costoEstimado = costoVidrio + costoMoldura + costoOtros
-let precioFinal = parseInt (2 * costoEstimado)
+  let costoEstimado = costoVidrio + costoMoldura + costoOtros
+  let precioFinal = parseInt(2 * costoEstimado)
 
-let precioRedondeado = Math.ceil(precioFinal / 50) * 50;
+  let precioRedondeado = Math.ceil(precioFinal / 50) * 50;
+
+  console.log(
+  `medidas: ${largo}x${ancho} cm.\n 
+  costo del vidrio: ${parseInt(costoVidrio)}\n 
+  perimetro: ${perimetro}\n 
+  costo de moldura: ${costoMoldura}\n 
+  precio final: ${precioRedondeado}`
+);
 
 
-console.log ("medidas: " + largo + "x" + ancho + " cm.");
-console.log ("costo del vidrio: " + " " + parseInt(costoVidrio));
-console.log ("perimetro: " + perimetro)
-console.log ("costo de moldura: " + costoMoldura)
-console.log ("precio final: " + precioRedondeado)
-
-alert("El precio para tu marco de " + largo + " x " + ancho +" es de " + precioRedondeado + " pesos" )
+  alert(`El precio para tu marco de ${largo} x ${ancho} es de ${precioRedondeado} pesos`)
 
 }
 
